6067fd37974dcd541804f525939091666e6a6fc9
Dirk <dirk@popskorn.de>
2015-04-20 17:00:07 +0200

    Aufgabe 2 fertig.

--- Assignment1/HelloWorldGraphical/HelloWorldGraphical.java
+++ Assignment1/HelloWorldGraphical/HelloWorldGraphical.java
@@ -1,23 +1,20 @@
-//import Exceptions
 import java.io.IOException;
-//import Java Swing
 import javax.swing.*;
-//import Event Listeners
 import java.awt.event.*;
-
 import java.awt.Component;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 
 public class HelloWorldGraphical extends JFrame implements ActionListener{
 
-	//global variable which saves text label
-	JLabel label;
 	//global variables for buttons
-	JButton deutschButton;
+	JButton germanButton;
 	JButton englishButton;
-	JButton francaisButton;
+	JButton frenchButton;
 	JButton quitButton;
+
+	//global variable which saves text label
+	JLabel greetingLabel;
 
 	public HelloWorldGraphical()
 	{
@@ -27,59 +24,59 @@ public class HelloWorldGraphical extends JFrame implements ActionListener{
 		JPanel languageSelectionPanel = new JPanel();
 		languageSelectionPanel.setLayout(new BoxLayout(languageSelectionPanel, BoxLayout.PAGE_AXIS));
 
+		// Generating the buttons and adding them to the first panel
 		germanButton = new JButton("Deutsch");
 		germanButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 		languageSelectionPanel.add(Box.createRigidArea(new Dimension(0, 30)));
 		languageSelectionPanel.add(germanButton);
+		germanButton.addActionListener(this);
 
 		englishButton = new JButton("English");
 		englishButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 		languageSelectionPanel.add(Box.createRigidArea(new Dimension(0, 30)));
 		languageSelectionPanel.add(englishButton);
+		englishButton.addActionListener(this);		
 
 		frenchButton = new JButton("Francais");
 		frenchButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 		languageSelectionPanel.add(Box.createRigidArea(new Dimension(0, 30)));
 		languageSelectionPanel.add(frenchButton);
+		frenchButton.addActionListener(this);		
 
+		// The greeting label and extra panel
 		JPanel greetingPanel = new JPanel();
 		greetingPanel.setLayout(new BoxLayout(greetingPanel, BoxLayout.LINE_AXIS));
-		greetingLabel = new JLabel("XXXXXXXXX");
+		greetingLabel = new JLabel("");
 		greetingLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
 		greetingPanel.add(Box.createRigidArea(new Dimension(40, 0)));
 		greetingPanel.add(greetingLabel);
 		
-		JPanel panel3 = new JPanel();
-		panel3.setLayout(new BoxLayout(panel3, BoxLayout.PAGE_AXIS));
+		JPanel quitPanel = new JPanel();
+		quitPanel.setLayout(new BoxLayout(quitPanel, BoxLayout.PAGE_AXIS));
 		quitButton = new JButton("Beenden");
 		quitButton.setAlignmentX(Component.RIGHT_ALIGNMENT);
-		panel3.add(quitButton);
-		panel3.add(Box.createRigidArea(new Dimension(20,20)));
-
-		//add listener to buttons
-		deutschButton.addActionListener(this);		
-		englishButton.addActionListener(this);		
-		francaisButton.addActionListener(this);		
+		quitPanel.add(quitButton);
+		quitPanel.add(Box.createRigidArea(new Dimension(20,20)));
 		quitButton.addActionListener(this);
-		
-		//adding panel to frame
-		this.add(panel, BorderLayout.NORTH);
+
+		//adding the panels to the frame
+		this.add(languageSelectionPanel, BorderLayout.NORTH);
 		this.add(greetingPanel, BorderLayout.CENTER);
-		this.add(panel3, BorderLayout.PAGE_END);
+		this.add(quitPanel, BorderLayout.PAGE_END);
 	}
 
 	//method finds out which button is pressed and corresponds to result
 	public void actionPerformed (ActionEvent ae)
 	{
-		if (ae.getSource() == this.deutschButton)
-			label.setText("Guten Tag!");
+		if (ae.getSource() == this.germanButton)
+			greetingLabel.setText("Guten Tag!");
 		else if (ae.getSource() == this.englishButton)
-			label.setText("Hello!");
-		else if (ae.getSource() == this.francaisButton)
-			label.setText("Bonjour!");
+			greetingLabel.setText("Hello!");
+		else if (ae.getSource() == this.frenchButton)
+			greetingLabel.setText("Bonjour!");
 		else if (ae.getSource() == this.quitButton)
 		{
-			label.setText("Beenden...");
+			greetingLabel.setText("Terminating Program...");
 			System.exit(0);
 		}
 	}
