9fa0457946e213894ad7bc17bb02f68e273f7bcb
Dirk <dirk@popskorn.de>
2015-04-20 16:40:13 +0200

    Aufgabe 2

--- Assignment1/HelloWorldGraphical/HelloWorldGraphical.java
+++ Assignment1/HelloWorldGraphical/HelloWorldGraphical.java
@@ -1,38 +1,97 @@
+//import Exceptions
+import java.io.IOException;
+//import Java Swing
+import javax.swing.*;
+//import Event Listeners
+import java.awt.event.*;
+
+import java.awt.Component;
 import java.awt.BorderLayout;
-import java.awt.Container;
-import java.awt.FlowLayout;
-import java.awt.BoxLayout;
+import java.awt.Dimension;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+public class HelloWorldGraphical extends JFrame implements ActionListener{
 
+	//global variable which saves text label
+	JLabel label;
+	//global variables for buttons
+	JButton deutschButton;
+	JButton englishButton;
+	JButton francaisButton;
+	JButton quitButton;
 
-public class HelloWorldGraphical {
-	  
-	public static void main(String[] args) {
-		
-		JButton germanButton = new JButton("Deutsch");
-		JButton englishButton = new JButton("English");
-		JButton frenchButton = new JButton("Frencais");
-
-		JFrame frame = new JFrame("Hello GUI");		
-		
-		Container frameContents = frame.getContentPane();
+	//constructor for UI-App
+	public HelloWorldGraphical()
+	{
+		//generating the frame and panel
+		this.setTitle("Hello GUI");
+		this.setSize(400,400);
 		
 
 		JPanel panel = new JPanel();
-		frameContents.add(panel);
 		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
-		panel.add(germanButton);
+
+		//generating the buttons
+		deutschButton = new JButton("Deutsch");
+		deutschButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+		panel.add(Box.createRigidArea(new Dimension(0, 30)));
+		panel.add(deutschButton);
+
+		englishButton = new JButton("English");
+		englishButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+		panel.add(Box.createRigidArea(new Dimension(0, 30)));
 		panel.add(englishButton);
-		panel.add(frenchButton);
 
+		francaisButton = new JButton("Francais");
+		francaisButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+		panel.add(Box.createRigidArea(new Dimension(0, 30)));
+		panel.add(francaisButton);
 
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(200, 300);
-		frame.setLocationRelativeTo(null); // Center on screen
-		// frame.pack(); // Überschreibt die Größeneinstellung mit setSize!
-		frame.setVisible(true);
+		JPanel panel2 = new JPanel();
+		panel2.setLayout(new BoxLayout(panel2, BoxLayout.LINE_AXIS));
+		label = new JLabel("XXXXXXXXX");
+		label.setAlignmentX(Component.LEFT_ALIGNMENT);
+		panel2.add(Box.createRigidArea(new Dimension(40, 0)));
+		panel2.add(label);
+		
+		JPanel panel3 = new JPanel();
+		panel3.setLayout(new BoxLayout(panel3, BoxLayout.PAGE_AXIS));
+		quitButton = new JButton("Beenden");
+		quitButton.setAlignmentX(Component.RIGHT_ALIGNMENT);
+		panel3.add(quitButton);
+		panel3.add(Box.createRigidArea(new Dimension(20,20)));
+
+		//add listener to buttons
+		deutschButton.addActionListener(this);		
+		englishButton.addActionListener(this);		
+		francaisButton.addActionListener(this);		
+		quitButton.addActionListener(this);
+		
+		//adding panel to frame
+		this.add(panel, BorderLayout.NORTH);
+		this.add(panel2, BorderLayout.CENTER);
+		this.add(panel3, BorderLayout.PAGE_END);
+	}
+
+	//method finds out which button is pressed and corresponds to result
+	public void actionPerformed (ActionEvent ae)
+	{
+		if (ae.getSource() == this.deutschButton)
+			label.setText("Guten Tag!");
+		else if (ae.getSource() == this.englishButton)
+			label.setText("Hello!");
+		else if (ae.getSource() == this.francaisButton)
+			label.setText("Bonjour!");
+		else if (ae.getSource() == this.quitButton)
+		{
+			label.setText("Beenden...");
+			System.exit(0);
+		}
+	}
+
+	public static void main (String [] args)
+	{
+		//generate new application and set it visible
+		HelloWorldGraphical app = new HelloWorldGraphical();
+		app.setVisible(true);
 	}
 }
